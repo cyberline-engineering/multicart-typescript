b1f45993ee510824010fe649943018b9e1393697
Den <den@multicartshop.com>
2022-10-07 10:42:12 +0300

    init demo app

--- examples/multicart-api-demo/src/App.vue
+++ examples/multicart-api-demo/src/App.vue
@@ -1,12 +1,26 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
